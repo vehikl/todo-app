000d1fc9b76ad191866a3036c2f69c787dbb1fc4
dfilter <dirk_mail@protonmail.com>
2022-02-02 16:10:34 -0500

    WIP

--- src/index.ts
+++ src/index.ts
@@ -3,33 +3,37 @@ import Todos from "./Todos";
 
 // https://github.com/SBoudrias/Inquirer.js#reactive-interface
 // https://github.com/SBoudrias/Inquirer.js/blob/master/packages/inquirer/examples/recursive.js
-inquirer
-    .prompt([
-        {
-            type: 'input',
-            name: 'todoValue',
-            message: "Enter your TODO",
-        },
-        {
-            type: 'input',
-            name: 'todoList',
-            message: "2. Display TODO List",
-        },
-    ])
-    .then((answers) => {
+const questions = [
+    {
+        type: 'input',
+        name: 'todoValue',
+        message: "Enter your TODO",
+    },
+    {
+        type: 'input',
+        name: 'todoList',
+        message: "2. Display TODO List",
+    },
+];
+
+(async () => {
+    try {
+
+        const answers = await inquirer.prompt(questions)
         const mytodos = new Todos();
         mytodos.addTodo(answers.todoValue);
-
+    
         if (answers.todolist === 'yes') {
             console.log(mytodos.getTodos());
         } else {
             console.log('bye bye then');
         }
-    })
-    .catch((error) => {
+    }
+    catch(error: any) {
         if (error.isTtyError) {
             // Prompt couldn't be rendered in the current environment
         } else {
             // Something else went wrong
         }
-    });
+    }
+})()
