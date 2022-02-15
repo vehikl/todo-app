574cffcc66c586a39640305c055687ba9e982a13
dfilter <dirk_mail@protonmail.com>
2022-02-15 15:27:34 -0500

    init TypeORM

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
+import 'reflect-metadata';
+import {createConnection} from 'typeorm';
 import inquirer from 'inquirer'
 
+import {User} from './entity/User';
 import Todos from './Todos'
 
 enum Operation {
@@ -98,3 +101,21 @@ const run = async () => {
         await run()
     }
 })()
+
+// createConnection().then(async connection => {
+
+//     console.log('Inserting a new user into the database...');
+//     const user = new User();
+//     user.firstName = 'Timber';
+//     user.lastName = 'Saw';
+//     user.age = 25;
+//     await connection.manager.save(user);
+//     console.log('Saved a new user with id: ' + user.id);
+
+//     console.log('Loading users from the database...');
+//     const users = await connection.manager.find(User);
+//     console.log('Loaded users: ', users);
+
+//     console.log('Here you can setup and run express/koa/any other framework.');
+
+// }).catch(error => console.log(error));
